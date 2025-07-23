6c9c405099574d114cda1b18375a96e63c119632
Lucas Pilati <pilatilucas@gmail.com>
2025-07-23 00:34:42 -0300

    structure backend

---
+++ api-mock/src/server.js
@@ -0,0 +1,35 @@
+const jsonServer = require("json-server");
+const auth = require('json-server-auth');
+const cors = require('cors');
+
+
+const app = jsonServer.create();
+const router = jsonServer.router('./src/data/db.json');
+const middlewares = jsonServer.defaults();
+
+app.db = router.db;
+
+app.use(cors());
+app.use(middlewares);
+app.use(jsonServer.bodyParser);
+
+const rules = auth.rewriter({
+    users: 600,
+    orders: 660,
+    cart: 660,
+    products: 660,
+    saleProducts: 660,
+    notifications: 660,
+});
+
+app.use(rules);
+app.use(auth);
+app.use(router);
+
+console.log('Rotas disponiveis')
+router.db.getState();
+
+
+app.listen(3001, () => {
+    console.log('rodando...')
+})
