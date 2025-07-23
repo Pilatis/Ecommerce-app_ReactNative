3e55133c5d8ae939eb93b6834f8cac8837464cb7
Lucas Pilati <pilatilucas@gmail.com>
2025-07-23 00:35:50 -0300

    resting commits

--- app/(tabs)/profile.tsx
+++
@@ -1,7 +0,0 @@
-
-
-export default function Profile() {
-  <div>
-    Profile
-  </div>
-}
--- src/types/fadeType.ts
+++
@@ -1,1 +0,0 @@
-export type FadeType = 'FadeIn' | 'FadeInDown' | 'FadeInLeft' | 'FadeInRight' | 'FadeInUp';
